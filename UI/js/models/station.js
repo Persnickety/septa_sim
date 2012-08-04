c119a4e4510916e296de723ee7f5e674db884fa8
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-08-04 19:58:58 -0400

    Add station model and collection; Add septa map view that renders to a raphael canvas; Add some dummy data to test with

--- UI/js/models/station.js
+++ UI/js/models/station.js
@@ -1,15 +1,15 @@
-SeptaSim = SeptaSim || {}
+var SeptaSim = SeptaSim || {};
 
 (function(S)
 {
 	S.Station = Backbone.Model.extend({
-					idAttribute: "stopid"
+					idAttribute: "stop_id"
 					
 				});
 				
 	S.StationCollection = Backbone.Collection.extend({
-					url: ''
-					model: S.Station
+					url: '',
+					model: S.Station,
 					getInterpolatedLocation: function(fromStation, toStation, timeInterval){
 												
 												return getInterpolatedLocation(fromStation.get('stop_lat'),  fromStation.get('stop_lon'), toStation.get('stop_lat'), toStation.get('stop_lon'), timeInterval);
