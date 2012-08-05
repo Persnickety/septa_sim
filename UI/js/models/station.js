84d8a9574e136b80cde5fffe2ef51e4ab649de4b
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-08-05 10:22:17 -0400

    whitespace

--- UI/js/models/station.js
+++ UI/js/models/station.js
@@ -2,20 +2,17 @@ var SeptaSim = SeptaSim || {};
 
 (function(S)
 {
+
 	S.Station = Backbone.Model.extend({
-					idAttribute: "stop_id"
-					
-				});
+		idAttribute: "stop_id"
+	});
 				
 	S.StationCollection = Backbone.Collection.extend({
-					url: '',
-					model: S.Station,
-					getInterpolatedLocation: function(fromStation, toStation, timeInterval){
-												
-												return getInterpolatedLocation(fromStation.get('stop_lat'),  fromStation.get('stop_lon'), toStation.get('stop_lat'), toStation.get('stop_lon'), timeInterval);
-												
-											}
-				});
-
+		url: '',
+		model: S.Station,
+		getInterpolatedLocation: function(fromStation, toStation, timeInterval){
+			return getInterpolatedLocation(fromStation.get('stop_lat'),  fromStation.get('stop_lon'), toStation.get('stop_lat'), toStation.get('stop_lon'), timeInterval);
+		}
+	});
 	
 })(SeptaSim);
