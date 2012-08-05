293ae62bfa0eb7c8e92ba954fffa97a828cb20fa
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-08-05 01:54:14 -0400

    Add pause and play buttons for the simulation slider

--- UI/js/views/septamap.js
+++ UI/js/views/septamap.js
@@ -5,16 +5,37 @@ var SeptaSim = SeptaSim || {};
 	S.TimeSliderView = Backbone.View.extend({
 		initialize: function() {
 			var $slider = $('#septa-time-slider');
+			var $play = $('#septa-play-button');
+			var $pause = $('#septa-pause-button');
+
 			this.trainCollection = this.options.trainCollection;
 			this.stationCollection = this.options.stationCollection;
+
 			$slider.change(_.bind(this.onSliderChange, this));
+			$play.click(_.bind(this.startSimulation, this));
+			$pause.click(_.bind(this.pauseSimulation, this));
 
-			var play = function() {
-				$slider.val($slider.val()*1 + 1).change();
-				_.delay(play, 100);
+			this.startSimulation();
+		},
+
+		startSimulation: function() {
+			if (!this.isStarted) {
+				this.isStarted = true
+				this.continueSimulation();
 			}
+		},
 
-			play();
+		continueSimulation: function() {
+			var $slider = $('#septa-time-slider');
+			$slider.val($slider.val()*1 + 1).change();
+			this.simContinuationId = _.delay(_.bind(this.continueSimulation, this), 100);
+		},
+
+		pauseSimulation: function() {
+			if (this.isStarted) {
+				clearTimeout(this.simContinuationId);
+				this.isStarted = false;
+			}
 		},
 
 		onSliderChange: function(evt) {
