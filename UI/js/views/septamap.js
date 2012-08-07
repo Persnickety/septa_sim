fa7e0601d9c9f884b42d5e96d3ae7162d2e8e963
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-08-07 00:30:51 -0400

    whitespace in the models and views

--- UI/js/views/septamap.js
+++ UI/js/views/septamap.js
@@ -3,261 +3,260 @@ var SeptaSim = SeptaSim || {};
 (function(S, $)
 {
 
-	S.VehicleMarkerView = Backbone.View.extend({
-		initialize: function() {
-			this.paper = this.options.paper;
-			this.mapView = this.options.mapView;
-			
-			this.model.bind('change', this.onVehicleChange, this);
-			this.model.bind('select', this.onVehicleSelect, this);
-			
-			this.marker = null;
-		},
+  S.VehicleMarkerView = Backbone.View.extend({
+    initialize: function() {
+      this.paper = this.options.paper;
+      this.mapView = this.options.mapView;
 
-		onVehicleChange: function() {
-			this.render();
-		},
+      this.model.bind('change', this.onVehicleChange, this);
+      this.model.bind('select', this.onVehicleSelect, this);
 
-		onVehicleSelect: function() {
-			if (this.model.selected) {
-				this.flashMarker();
-			}
-			this.render();
-		},
+      this.marker = null;
+    },
 
-		onVehicleClick: function() {
-			if (!this.model.selected) {
-				this.model.select();
-			} else {
-				this.model.deselect();
-			}
-		},
+    onVehicleChange: function() {
+      this.render();
+    },
 
-		flashMarker: function() {
+    onVehicleSelect: function() {
+      if (this.model.selected) {
+        this.flashMarker();
+      }
+      this.render();
+    },
 
-		},
+    onVehicleClick: function() {
+      if (!this.model.selected) {
+        this.model.select();
+      } else {
+        this.model.deselect();
+      }
+    },
 
-		createMarker: function(coords, isOutbound) {
-			var color = (isOutbound ? '#00FF00': 'Red')
-			this.marker = this.paper.circle(coords.x, coords.y, 0.005).attr({
-						'stroke': color,
-						'stroke-width': 1,
-						'fill': color,
-						'r': '0.005'
-					});
+    flashMarker: function() {
 
-			if(this.model.selected){
-				var ox = this.marker.attr("cx");
-				var oy = this.marker.attr("cy");
-				this.marker.attr('r','0.007');
-				this.marker.attr('stroke-width','2');
-				this.marker.attr('fill','yellow');
-			};
-			
-			var onVehicleClick = _.bind(this.onVehicleClick, this);
-			$(this.marker.node).click(onVehicleClick);
-		},
+    },
 
-		createText: function(coords, isOutbound, displayString) {
-			this.text = this.paper.text(coords.x, coords.y, displayString).attr({
-						stroke: 'none',
-						'text-size': 1,
-						fill: (this.model.selected ? 'yellow' : (isOutbound ? 'green': 'red'))
-					});
-			this.text.transform('s0.002,0.002,' + coords.x + ',' + coords.y + 't15,0');
-		},
+    createMarker: function(coords, isOutbound) {
+      var color = (isOutbound ? '#00FF00': 'Red')
+      this.marker = this.paper.circle(coords.x, coords.y, 0.005).attr({
+            'stroke': color,
+            'stroke-width': 1,
+            'fill': color,
+            'r': '0.005'
+          });
 
-		updateMarker: function(coords, isOutbound, displayString) {
-			var color = (isOutbound ? '#00FF00': 'Red')
-			this.marker.attr({
-						cx: coords.x,
-						cy: coords.y,
-						'stroke': color,
-						'fill': color,
-						'r': '0.005'
-					});
+      if(this.model.selected){
+        var ox = this.marker.attr("cx");
+        var oy = this.marker.attr("cy");
+        this.marker.attr('r','0.007');
+        this.marker.attr('stroke-width','2');
+        this.marker.attr('fill','yellow');
+      };
 
-			if(this.model.selected){
-				var ox = this.marker.attr("cx");
-				var oy = this.marker.attr("cy");
-				this.marker.attr('r','0.007');
-				this.marker.attr('stroke-width','2');
-				this.marker.attr('fill','yellow');
-			};
-		},
+      var onVehicleClick = _.bind(this.onVehicleClick, this);
+      $(this.marker.node).click(onVehicleClick);
+    },
 
-		updateText: function(coords, isOutbound) {
-			this.text.attr({
-						x: coords.x,
-						y: coords.y,
-						transform: '',
-						fill: (this.model.selected ? 'yellow' : (isOutbound ? 'green': 'red'))
-					});
-			this.text.transform('s0.002,0.002,' + coords.x + ',' + coords.y + 't15,0');
-		},
+    createText: function(coords, isOutbound, displayString) {
+      this.text = this.paper.text(coords.x, coords.y, displayString).attr({
+            stroke: 'none',
+            'text-size': 1,
+            fill: (this.model.selected ? 'yellow' : (isOutbound ? 'green': 'red'))
+          });
+      this.text.transform('s0.002,0.002,' + coords.x + ',' + coords.y + 't15,0');
+    },
 
-		removeMarker: function() {
-			if (this.marker) {
-				this.marker.remove();
-				this.marker = null;
-			}
-		},
+    updateMarker: function(coords, isOutbound, displayString) {
+      var color = (isOutbound ? '#00FF00': 'Red')
+      this.marker.attr({
+            cx: coords.x,
+            cy: coords.y,
+            'stroke': color,
+            'fill': color,
+            'r': '0.005'
+          });
 
-		removeText: function() {
-			if (this.text) {
-				this.text.remove();
-				this.text = null;
-			}
-		},
+      if(this.model.selected){
+        var ox = this.marker.attr("cx");
+        var oy = this.marker.attr("cy");
+        this.marker.attr('r','0.007');
+        this.marker.attr('stroke-width','2');
+        this.marker.attr('fill','yellow');
+      };
+    },
 
-		render: function() {
-			if (this.model.get('active')) {
-				var trainLocation = this.model.get('location');
-				var coords = this.mapView.toMapCoords(trainLocation.lat, trainLocation.lon);
-				var is_outbound = this.model.get('outbound?');
-				var block_id = this.model.get('block_id');
+    updateText: function(coords, isOutbound) {
+      this.text.attr({
+            x: coords.x,
+            y: coords.y,
+            transform: '',
+            fill: (this.model.selected ? 'yellow' : (isOutbound ? 'green': 'red'))
+          });
+      this.text.transform('s0.002,0.002,' + coords.x + ',' + coords.y + 't15,0');
+    },
 
-				// If we don't have a marker yet...
-				if (this.marker === null) {
-					this.createMarker(coords, is_outbound);
-//					this.createText(coords, is_outbound, block_id);
+    removeMarker: function() {
+      if (this.marker) {
+        this.marker.remove();
+        this.marker = null;
+      }
+    },
 
-				// If we already have a marker...
-				} else {
-					this.updateMarker(coords, is_outbound);
-//					this.updateText(coords, is_outbound, block_id);
-				}
-				
-				var outbound = this.model.get('outbound');
-				var routeName = this.model.get('routeName');
-				var nextStation = this.model.get('nextStation');
-				var arrivalTime = this.model.get('arrivalTime');
-				
-				var $tooltip = 'Route: ' + routeName + '\n' + 'Next Station: ' + nextStation + '\n' + 'Arrival Time: ' + arrivalTime;
-				if(outbound){
-					$tooltip += '\nOutbound Train';
-				}
-				else {
-					$tooltip += '\nInbound Train';
-				}
-				
-				addTip(this.marker.node, $tooltip);
+    removeText: function() {
+      if (this.text) {
+        this.text.remove();
+        this.text = null;
+      }
+    },
 
-			} else {
-				this.removeMarker();
-//				this.removeText();
-			}
-		}
-	});
-	
-	S.SeptaMapView = Backbone.View.extend({
-						initialize: function() {
+    render: function() {
+      if (this.model.get('active')) {
+        var trainLocation = this.model.get('location');
+        var coords = this.mapView.toMapCoords(trainLocation.lat, trainLocation.lon);
+        var is_outbound = this.model.get('outbound?');
+        var block_id = this.model.get('block_id');
 
-							var w = $('#septa-canvas').width();
-							var h = $('#septa-canvas').height();
+        // If we don't have a marker yet...
+        if (this.marker === null) {
+          this.createMarker(coords, is_outbound);
+//          this.createText(coords, is_outbound, block_id);
 
-							this.paper = ScaleRaphael('septa-canvas', w, h);
-							this.width = w;
-							this.height = h;
+        // If we already have a marker...
+        } else {
+          this.updateMarker(coords, is_outbound);
+//          this.updateText(coords, is_outbound, block_id);
+        }
 
-							this.stationCollection = this.options.stationCollection;
-							this.trainCollection = this.options.trainCollection;
+        var outbound = this.model.get('outbound');
+        var routeName = this.model.get('routeName');
+        var nextStation = this.model.get('nextStation');
+        var arrivalTime = this.model.get('arrivalTime');
 
-						},
+        var $tooltip = 'Route: ' + routeName + '\n' + 'Next Station: ' + nextStation + '\n' + 'Arrival Time: ' + arrivalTime;
+        if(outbound){
+          $tooltip += '\nOutbound Train';
+        }
+        else {
+          $tooltip += '\nInbound Train';
+        }
 
-						// Returns a pair of x, y coordinates.
-						toMapCoords: function(lat, lon) {
-							var coords = {
-								x: lon,
-								y: -lat*2
-							};
-							return coords;
-						},
+        addTip(this.marker.node, $tooltip);
 
-						setViewBox: function(minLat, minLon, maxLat, maxLon) {
-							this.paper.setViewBox(minLon, -maxLat*2, (maxLon - minLon), (maxLat - minLat)*2, true);
-						},
-	          
-						render: function() {
+      } else {
+        this.removeMarker();
+//        this.removeText();
+      }
+    }
+  });
 
-							var stationMarkers = {};
-							var paper = this.paper;
-							var mapSet = this.mapSet;
-							var toMapCoords = this.toMapCoords;
+  S.SeptaMapView = Backbone.View.extend({
+            initialize: function() {
 
-//							$.ajax({
-//								type: 'GET',
-//								url: 'http://septasim.onlinewebshop.net/stations_by_route.php',
-//								success: function(data) {
-//										console.log(data);
-//									}
-//							);
+              var w = $('#septa-canvas').width();
+              var h = $('#septa-canvas').height();
 
-							var lats = this.stationCollection.pluck('stop_lat');
-							var lons = this.stationCollection.pluck('stop_lon');
-							this.setViewBox(_.min(lats), _.min(lons), _.max(lats), _.max(lons));
+              this.paper = ScaleRaphael('septa-canvas', w, h);
+              this.width = w;
+              this.height = h;
 
-							this.drawRoutes(STATIONS);
+              this.stationCollection = this.options.stationCollection;
+              this.trainCollection = this.options.trainCollection;
 
-							this.stationCollection.each(function(station) {
-								var coords = toMapCoords(station.get('stop_lat'), station.get('stop_lon'))
-								var marker = paper.circle(coords.x, coords.y, 0.004).attr({
-									'stroke': 'black',
-									'stroke-width': 1,
-									'fill': 'white'
-								});
+            },
 
-								addTip(marker.node, station.get('stop_name'));
-								
-								stationMarkers[station] = marker;
-							});
+            // Returns a pair of x, y coordinates.
+            toMapCoords: function(lat, lon) {
+              var coords = {
+                x: lon,
+                y: -lat*2
+              };
+              return coords;
+            },
 
-							this.stationMarkers = stationMarkers;
+            setViewBox: function(minLat, minLon, maxLat, maxLon) {
+              this.paper.setViewBox(minLon, -maxLat*2, (maxLon - minLon), (maxLat - minLat)*2, true);
+            },
 
-							var vehicleMarkerViews = [];
-							var self = this;
-							this.trainCollection.each(function(train) {
-								var view = new S.VehicleMarkerView({
-									paper: paper,
-									mapView: self,
-									model: train
-								});
-								vehicleMarkerViews[train] = view;
-							});
-							this.vehicleMarkerViews = vehicleMarkerViews;
-						},
+            render: function() {
 
-						drawRoutes: function(stations_by_route) {
-							var stationCollection = this.stationCollection;
-							var toMapCoords = this.toMapCoords;
-							var paper = this.paper;
+              var stationMarkers = {};
+              var paper = this.paper;
+              var mapSet = this.mapSet;
+              var toMapCoords = this.toMapCoords;
 
-							var route_to_color = {'AIR':'#91456C', 'CHE':'#94763C', 'CHW':'#00B4B2','DOY':'#775B49','ELW':'#007CC8','FOX':'#FF823D','NOR':'#EE4C69','PAO':'#20825C','CYN':'#6F549E','TRE':'#F683C9','WAR':'#F7AF42','WIL':'#8AD16B','WTR':'#5D5EBC'};
-							
-							for (route_id in stations_by_route) {
-								var pathCoords = '';
+//              $.ajax({
+//                type: 'GET',
+//                url: 'http://septasim.onlinewebshop.net/stations_by_route.php',
+//                success: function(data) {
+//                    console.log(data);
+//                  }
+//              );
 
-								_.each(stations_by_route[route_id], function(stop_id) {
-									var station = stationCollection.get(stop_id);
-									coords = toMapCoords(station.get('stop_lat'), station.get('stop_lon'));
+              var lats = this.stationCollection.pluck('stop_lat');
+              var lons = this.stationCollection.pluck('stop_lon');
+              this.setViewBox(_.min(lats), _.min(lons), _.max(lats), _.max(lons));
 
-									if (pathCoords === '') {
-										pathCoords += 'M' + coords.x + ' ' + coords.y;
-									} else {
-										pathCoords += 'L' + coords.x + ' ' + coords.y;
-									}
-								});
+              this.drawRoutes(STATIONS);
 
-								paper.path(pathCoords).attr({
-									'stroke-width': 4,
-									'stroke': route_to_color[route_id]
-								});
-								
-								
-							}
-						}
-					});
-	
-					
+              this.stationCollection.each(function(station) {
+                var coords = toMapCoords(station.get('stop_lat'), station.get('stop_lon'))
+                var marker = paper.circle(coords.x, coords.y, 0.004).attr({
+                  'stroke': 'black',
+                  'stroke-width': 1,
+                  'fill': 'white'
+                });
+
+                addTip(marker.node, station.get('stop_name'));
+
+                stationMarkers[station] = marker;
+              });
+
+              this.stationMarkers = stationMarkers;
+
+              var vehicleMarkerViews = [];
+              var self = this;
+              this.trainCollection.each(function(train) {
+                var view = new S.VehicleMarkerView({
+                  paper: paper,
+                  mapView: self,
+                  model: train
+                });
+                vehicleMarkerViews[train] = view;
+              });
+              this.vehicleMarkerViews = vehicleMarkerViews;
+            },
+
+            drawRoutes: function(stations_by_route) {
+              var stationCollection = this.stationCollection;
+              var toMapCoords = this.toMapCoords;
+              var paper = this.paper;
+
+              var route_to_color = {'AIR':'#91456C', 'CHE':'#94763C', 'CHW':'#00B4B2','DOY':'#775B49','ELW':'#007CC8','FOX':'#FF823D','NOR':'#EE4C69','PAO':'#20825C','CYN':'#6F549E','TRE':'#F683C9','WAR':'#F7AF42','WIL':'#8AD16B','WTR':'#5D5EBC'};
+
+              for (route_id in stations_by_route) {
+                var pathCoords = '';
+
+                _.each(stations_by_route[route_id], function(stop_id) {
+                  var station = stationCollection.get(stop_id);
+                  coords = toMapCoords(station.get('stop_lat'), station.get('stop_lon'));
+
+                  if (pathCoords === '') {
+                    pathCoords += 'M' + coords.x + ' ' + coords.y;
+                  } else {
+                    pathCoords += 'L' + coords.x + ' ' + coords.y;
+                  }
+                });
+
+                paper.path(pathCoords).attr({
+                  'stroke-width': 4,
+                  'stroke': route_to_color[route_id]
+                });
+
+
+              }
+            }
+          });
+
+
 })(SeptaSim, jQuery);
-
